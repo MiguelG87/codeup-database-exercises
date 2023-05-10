f827a0ffb22ce8faa241730369395ea6b29db14a
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-05-10 16:12:16 -0500

    where exercises

---
+++ where_exercises.sql
@@ -0,0 +1,16 @@
+use employees;
+-- PART 1 --------------------------------------------------------------
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT * from employees WHERE last_name LIKE 'E%';
+
+SELECT * from employees WHERE last_name LIKE '%q%';
+
+-- PART 2 --------------------------------------------------------------
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+
+SELECT * from employees WHERE (last_name LIKE 'E%' OR last_name LIKE '%E');
+
+SELECT * from employees WHERE (last_name LIKE 'E%' AND last_name LIKE '%E');
+
+SELECT * from employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
